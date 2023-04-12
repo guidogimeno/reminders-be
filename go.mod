0eebfb6e41679c420638c9106855bd206c8b3d6a
guidogimeno <gimenoguido@gmail.com>
2023-04-11 22:08:06 -0300

    feat: add env variables

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/guidogimeno/reminders-be.git
 go 1.20
 
 require (
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -4,17 +4,32 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/guidogimeno/reminders-be.git/api"
+	"github.com/joho/godotenv"
 )
-
-const port = ":3000"
 
 func main() {
 	fmt.Println("Starting server...")
 
-	db, err := sql.Open("mysql", "root:codeblocks@tcp(localhost:3306)/remindersdb")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_DATABASE")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, dbPort, database)
+
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		panic(err.Error())
 	}
